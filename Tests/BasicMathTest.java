d0069ca9fcafa31311f8b470440169c11dacd09b
Fre-Nie <bjdfredrik.niemi@gmail.com>
2019-02-09 16:50:32 +0100

    Created Fredriks branch

---
+++ Tests/BasicMathTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasicMathTest {
+
+    @Test
+    public void div() {
+        System.out.println("hej");
+    }
+}
---
+++ src/BasicMath.java
@@ -0,0 +1,16 @@
+public class BasicMath {
+        public int div(int a, int b) {
+            return a/b;
+        }
+
+        public int add(int a, int b)
+        {
+            return a+b;
+        }
+
+        public int multiply(int a, int b){
+            return a*b;
+        }
+}
+
+
