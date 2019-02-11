91602be60e52dfba6eba8829c2c05605ef10a88e
Torgny Lyon <torgny@abc.se>
2019-02-11 16:42:57 +0100

    Skapa tomma skelett.

---
+++ test/MyMathTest.java
@@ -0,0 +1,19 @@
+/*
+ * MyMathTest.java
+ *
+ * Torgny Lyon <torgny@abc.se>
+ */
+
+import org.junit.After;
+import org.junit.Before;
+
+public class MyMathTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
