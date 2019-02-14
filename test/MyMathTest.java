1176c86fa95d70d048c922a0843c7924a4255f37
Torgny Lyon <torgny@abc.se>
2019-02-14 19:13:33 +0100

    Skapa test för add av positiva tal vars summa ryms i int och skapa add()
    Skapa ett test som verifierar att två positiva tal som vart och ett
    får plats i en int och vars summa också får plats i en int adderas
    och resulterar i en korrekt summa.
    
    Skapa också en metod add() i produktionskoden med tillräcklig
    funktionalitet för att testet skall "visa grönt".
    
    Då metoden add() förväntas uppföra sig på samma vis som operatorn + i
    Java enligt §15.18.2 i The Java Language Specification - Java SE 8
    Edition och då testet syftar till att verifiera en operation som ej
    orsakar ett v.m.k. overflow, väljs argumenten så att de är större än vad
    som skulle få plats i en short men mindre än att overflow sker. Därmed
    inte sagt att argumenten inte representerar en ekvivalensklass som
    innefattar positiva tal mellan 0 och Short.MAX_VALUE. Argumenten
    representerar ekvivalensklassen 0-Integer.MAX_VALUE.

--- test/MyMathTest.java
+++ test/MyMathTest.java
@@ -4,16 +4,22 @@
  * Torgny Lyon <torgny@abc.se>
  */
 
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class MyMathTest {
+    private MyMath myMath;
 
     @Before
     public void setUp() {
+         myMath = new MyMath();
     }
 
-    @After
-    public void tearDown() {
+    @Test
+    public void addPositivesWithoutOverflow() {
+        int result = myMath.add(1000000000, 1000000000);
+
+        assertEquals(2000000000, result);
     }
 }
